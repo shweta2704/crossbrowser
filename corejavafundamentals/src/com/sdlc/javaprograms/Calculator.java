66a9ea788abc6816e441186f3f21bcb602acf957
Shweta D <sdavuluri102@gmail.com>
2019-02-18 11:37:20 -0500

    removed comments

--- corejavafundamentals/src/com/sdlc/javaprograms/Calculator.java
+++ corejavafundamentals/src/com/sdlc/javaprograms/Calculator.java
@@ -6,13 +6,11 @@ public class Calculator {
 	{
 		int c = a+b;
 		return c;
-		// code change in git
 	}
 	
 	public int sub(int a, int b)
 	{ int c = a-b;
 	return c;
-	//code change check in git
 	}
 
 }
